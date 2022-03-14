1283d0e7f856b6fbc005b4b85ccf38fccdf2d0be
TakExplores <tak.explores@gmail.com>
2022-03-14 14:16:04 +0800

    Understanding the problem and planning stage (no pseudocode yet)

---
+++ main.js
@@ -0,0 +1,12 @@
+/* Understand the problem
+Write a program that takes an input from a user of either rock, paper, or scissors, and plays 5 rounds of rock paper scissors with a computer. After every round, the winner is declared and a point is added to the winner
+
+- Does program have a UI? What will it look like? What functionality will the interface have? Sketch this out on paper
+    No UI.
+- What are the inputs? Will the user enter data or will you get input from somewhere else?
+    User has to enter data when prompted, 'Rock', 'Paper', or 'Scissors'
+- What’s the desired output?
+    A string that declares who the winner is, and a point added to the winner
+- **Given input X, what are the steps necessary to return output Y? -** this is the pseudocode
+*/
+
